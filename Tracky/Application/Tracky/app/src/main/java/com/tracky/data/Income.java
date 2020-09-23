192b7b2127259311c95267a0b9e6e92afbcfdecc
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-09-23 17:16:55 +0200

    Started working on the Android application source structure .

---
+++ Tracky/Application/Tracky/app/src/main/java/com/tracky/data/Income.java
@@ -0,0 +1,49 @@
+package com.tracky.data;
+
+import java.time.LocalDateTime;
+
+public class Income {
+
+    @Override
+    public String toString() {
+        return "Income{" +
+                "id=" + id +
+                ", amount=" + amount +
+                ", date=" + date +
+                '}';
+    }
+
+    private int id;
+    private int amount;
+    private LocalDateTime date;
+
+    public Income(int id, int amount, LocalDateTime date){
+        this.id = id;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+}
