3a96dffbeb860ddb0d7186ea129a200959039d41
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-09-23 18:19:15 +0200

    Added template module from the in_dev_modules folder to the application.

---
+++ Tracky/Application/Tracky/app/src/main/java/com/tracky/TemplateHandler/Template.java
@@ -0,0 +1,60 @@
+package com.tracky.TemplateHandler;
+
+import com.tracky.data.Group;
+
+public class Template {
+
+    private boolean isIncome;
+    private int id;
+    private int amount;
+    private int groupId;
+
+    public Template(boolean isIncome, int id, int amount, int groupId ) {
+        this.isIncome = isIncome;
+        this.id = id;
+        this.amount = amount;
+        this.groupId = groupId;
+    }
+
+    public boolean isIncome() {
+        return isIncome;
+    }
+
+    public void setIncome(boolean income) {
+        isIncome = income;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroup(Group group) {
+        this.groupId = group.getId();
+    }
+
+    @Override
+    public String toString() {
+        return "Template{" +
+                "isIncome=" + isIncome +
+                ", id=" + id +
+                ", amount=" + amount +
+                ", groupId=" + groupId +
+                '}';
+    }
+}
