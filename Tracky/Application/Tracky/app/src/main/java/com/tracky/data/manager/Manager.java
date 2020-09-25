b48fda400b06ef822da298ca4423c0eedc718745
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-09-26 00:33:33 +0200

    Added the DAO calls to the Manager class.

--- Tracky/Application/Tracky/app/src/main/java/com/tracky/data/manager/Manager.java
+++ Tracky/Application/Tracky/app/src/main/java/com/tracky/data/manager/Manager.java
@@ -1,25 +1,36 @@
 package com.tracky.data.manager;
 
-import com.tracky.TemplateHandler.Template;
+import androidx.room.Room;
+
+import com.tracky.MainActivity;
+import com.tracky.data.Template;
 import com.tracky.data.Expense;
 import com.tracky.data.Group;
 import com.tracky.data.Income;
+import com.tracky.data.base.AppDatabase;
 
 import java.util.Calendar;
 import java.util.Date;
 import java.util.ArrayList;
 
-public class Manager {
+public abstract class Manager{
 
-    ArrayList<Income> incomes=new ArrayList<>();
-    ArrayList<Expense> expenses=new ArrayList<>();
-    ArrayList<Template> templates=new ArrayList<>();
-    ArrayList<Group> groups=new ArrayList<>();
+    AppDatabase db = Room.databaseBuilder(MainActivity.context,
+            AppDatabase.class, "TrackyDB").build();
+
+    ArrayList<Income> incomes= db.incomeDao().selectAllIncome();
+    ArrayList<Expense> expenses= db.expenseDao().selectAllExpense();
+    ArrayList<Template> templates= db.templateDao().selectAllTemplate();
+    ArrayList<Group> groups= db.groupDao().selectAllGroup();
+
 
     //------------------------------------INCOMES----------------------------
+
+
     public void addIncome(int amount, String description){
         int id = incomes.size();
         incomes.add(new Income(id, amount, description, Calendar.getInstance().getTime() ));
+        db.incomeDao().insertIncome(new Income(id, amount, description, Calendar.getInstance().getTime()));
     }
 
     public void deleteIncome(int id){
@@ -27,6 +38,7 @@ public class Manager {
 
         for(; i< incomes.size(); i++){
             if( incomes.get(i).getId() == id){
+                db.incomeDao().deleteIncome(incomes.get(i));
                 incomes.remove(i);
                 break;
             }
@@ -34,6 +46,7 @@ public class Manager {
 
         for(; i < incomes.size(); i++){
             incomes.get(i).setId(incomes.get(i).getId() - 1);
+            db.incomeDao().updateIncome(incomes.get(i));
         }
     }
 
@@ -43,20 +56,25 @@ public class Manager {
                 income.setAmount(amount);
                 income.setDescription(description);
                 income.setDate(date);
+                db.incomeDao().updateIncome(income);
             }
         }
     }
 
-    //--------------------------------EXPANSE-------------------------------
+    //--------------------------------EXPENSE-------------------------------
+
+
     public void addExpense(int amount, String description, int groupId){
         int id = expenses.size();
         expenses.add(new Expense(id, amount, description, groupId, Calendar.getInstance().getTime() ));
+        db.expenseDao().insertExpense(new Expense(id, amount, description, groupId, Calendar.getInstance().getTime()));;
     }
 
     public void deleteExpense(int id){
         int i = 0;
         for(; i < expenses.size(); i++){
             if( expenses.get(i).getId() == id){
+                db.expenseDao().deleteExpense(expenses.get(i));
                 expenses.remove(i);
                 break;
             }
@@ -64,6 +82,7 @@ public class Manager {
 
         for(; i < expenses.size(); i++){
             expenses.get(i).setId(expenses.get(i).getId() - 1);
+            db.expenseDao().updateExpense(expenses.get(i));
         }
     }
 
@@ -74,14 +93,18 @@ public class Manager {
                 expense.setDescription(description);
                 expense.setGroup(group);
                 expense.setDate(date);
+                db.expenseDao().updateExpense(expense);
             }
         }
     }
 
     //--------------------------------------GROUPS------------------------
+
+
     public void addGroup(String name, int color){
         int id = groups.size();
         groups.add(new Group(id, name, color));
+        db.groupDao().insertGroup(new Group(id, name, color));
     }
 
     public void deleteGroup(int id){
@@ -89,6 +112,7 @@ public class Manager {
 
         for(; i< groups.size(); i++){
             if( groups.get(i).getId() == id){
+                db.groupDao().deleteGroup(groups.get(i));
                 groups.remove(i);
                 break;
             }
@@ -97,6 +121,7 @@ public class Manager {
 
         for(; i < groups.size(); i++){
             groups.get(i).setId(groups.get(i).getId() - 1);
+            db.groupDao().updateGroup(groups.get(i));
         }
     }
 
@@ -105,14 +130,18 @@ public class Manager {
             if (group.getId() == id) {
                 group.setName(name);
                 group.setColor(color);
+                db.groupDao().updateGroup(group);
             }
         }
     }
 
     //--------------------------------------Templates------------------------
+
+
     public void addTemplate(boolean isIncome, int amount, String description, int groupId){
         int id = templates.size();
         templates.add(new Template(isIncome, id, amount, description, groupId));
+        db.templateDao().insertTemplate(new Template(isIncome, id, amount, description, groupId));
     }
 
     public void deleteTemplate(int id){
@@ -120,6 +149,7 @@ public class Manager {
 
         for(; i< templates.size(); i++){
             if( templates.get(i).getId() == id){
+                db.templateDao().deleteTemplate(templates.get(i));
                 templates.remove(i);
                 break;
             }
@@ -127,6 +157,7 @@ public class Manager {
 
         for(; i < templates.size(); i++){
             templates.get(i).setId(templates.get(i).getId() - 1);
+            db.templateDao().updateTemplate(templates.get(i));
         }
     }
 
@@ -137,6 +168,7 @@ public class Manager {
                 template.setAmount(amount);
                 template.setGroup(group);
                 template.setDescription(description);
+                db.templateDao().updateTemplate(template);
             }
         }
     }
