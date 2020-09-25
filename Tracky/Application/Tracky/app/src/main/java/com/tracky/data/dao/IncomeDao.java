e74ae6c93c21ef5b5de662c1dd43b0266b4893ea
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-09-26 00:12:41 +0200

    Created IncomeDao interface.

---
+++ Tracky/Application/Tracky/app/src/main/java/com/tracky/data/dao/IncomeDao.java
@@ -0,0 +1,25 @@
+package com.tracky.data.dao;
+
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.tracky.data.Income;
+
+import java.util.List;
+
+public interface IncomeDao {
+    @Query("SELECT * FROM income")
+    List<Income> selectAllIncome();
+
+    @Insert
+    void insertIncome(Income income);
+
+    @Update
+    void updateIncome(Income income);
+
+    @Delete
+    void deleteIncome(Income income);
+}
+
