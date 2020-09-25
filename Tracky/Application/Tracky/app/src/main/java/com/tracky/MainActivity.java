00a62a51616846b827873a55ab92eb931b4bb28c
duszka-akos-attila <duszka.akos.attila@gmail.com>
2020-09-26 00:19:10 +0200

    Added necessary Application context variable to the MainActivity.

--- Tracky/Application/Tracky/app/src/main/java/com/tracky/MainActivity.java
+++ Tracky/Application/Tracky/app/src/main/java/com/tracky/MainActivity.java
@@ -2,13 +2,17 @@ package com.tracky;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = getApplicationContext();
     }
 }
