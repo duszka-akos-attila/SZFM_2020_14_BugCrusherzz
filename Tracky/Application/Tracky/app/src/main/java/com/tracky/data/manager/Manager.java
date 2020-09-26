c0d9c5360d94eaa30ce54edd5831b35cb6d5d4fb
raczandras <raczandras0204@gmail.com>
2020-09-26 14:51:01 +0200

    Optimized the methods of the manager class

--- Tracky/Application/Tracky/app/src/main/java/com/tracky/data/manager/Manager.java
+++ Tracky/Application/Tracky/app/src/main/java/com/tracky/data/manager/Manager.java
@@ -3,15 +3,15 @@ package com.tracky.data.manager;
 import androidx.room.Room;
 
 import com.tracky.MainActivity;
-import com.tracky.data.Template;
 import com.tracky.data.Expense;
 import com.tracky.data.Group;
 import com.tracky.data.Income;
+import com.tracky.data.Template;
 import com.tracky.data.base.AppDatabase;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Manager{
@@ -55,31 +55,22 @@ public class Manager{
     }
 
     public void deleteIncome(int id){
-        int i = 0;
 
-        for(; i< incomes.size(); i++){
-            if( incomes.get(i).getId() == id){
-                db.incomeDao().deleteIncome(incomes.get(i));
-                incomes.remove(i);
-                break;
-            }
-        }
+        db.incomeDao().deleteIncome(incomes.get(id));
+        incomes.remove(id);
 
-        for(; i < incomes.size(); i++){
+
+        for(int i = id; i < incomes.size(); i++){
             incomes.get(i).setId(incomes.get(i).getId() - 1);
             db.incomeDao().updateIncome(incomes.get(i));
         }
     }
 
-    public void editIncome(int id, int amount, String description, Date date){
-        for (Income income : incomes) {
-            if (income.getId() == id) {
-                income.setAmount(amount);
-                income.setDescription(description);
-                income.setDate(date);
-                db.incomeDao().updateIncome(income);
-            }
-        }
+    public void editIncome( Income income, int amount, String description, Date date){
+        income.setAmount(amount);
+        income.setDescription(description);
+        income.setDate(date);
+        db.incomeDao().updateIncome(income);
     }
 
     //--------------------------------EXPENSE-------------------------------
@@ -92,31 +83,22 @@ public class Manager{
     }
 
     public void deleteExpense(int id){
-        int i = 0;
-        for(; i < expenses.size(); i++){
-            if( expenses.get(i).getId() == id){
-                db.expenseDao().deleteExpense(expenses.get(i));
-                expenses.remove(i);
-                break;
-            }
-        }
+        db.expenseDao().deleteExpense(expenses.get(id));
+        expenses.remove(id);
 
-        for(; i < expenses.size(); i++){
+        for(int i = id; i < expenses.size(); i++){
             expenses.get(i).setId(expenses.get(i).getId() - 1);
             db.expenseDao().updateExpense(expenses.get(i));
         }
     }
 
-    public void editExpense(int id, int amount, String description, Group group, Date date){
-        for (Expense expense : expenses) {
-            if (expense.getId() == id) {
-                expense.setAmount(amount);
-                expense.setDescription(description);
-                expense.setGroup(group);
-                expense.setDate(date);
-                db.expenseDao().updateExpense(expense);
-            }
-        }
+    public void editExpense( Expense expense, int amount, String description, Group group, Date date){
+        expense.setAmount(amount);
+        expense.setDescription(description);
+        expense.setGroup(group);
+        expense.setDate(date);
+        db.expenseDao().updateExpense(expense);
+
     }
 
     //--------------------------------------GROUPS------------------------
@@ -129,31 +111,21 @@ public class Manager{
     }
 
     public void deleteGroup(int id){
-        int i = 0;
-
-        for(; i< groups.size(); i++){
-            if( groups.get(i).getId() == id){
-                db.groupDao().deleteGroup(groups.get(i));
-                groups.remove(i);
-                break;
-            }
-        }
+        db.groupDao().deleteGroup(groups.get(id));
+        groups.remove(id);
 
 
-        for(; i < groups.size(); i++){
+        for(int i = id; i < groups.size(); i++){
             groups.get(i).setId(groups.get(i).getId() - 1);
             db.groupDao().updateGroup(groups.get(i));
         }
     }
 
-    public void editGroup(int id, String name, int color){
-        for (Group group : groups) {
-            if (group.getId() == id) {
-                group.setName(name);
-                group.setColor(color);
-                db.groupDao().updateGroup(group);
-            }
-        }
+    public void editGroup(Group group, String name, int color){
+
+        group.setName(name);
+        group.setColor(color);
+        db.groupDao().updateGroup(group);
     }
 
     //--------------------------------------Templates------------------------
@@ -166,31 +138,29 @@ public class Manager{
     }
 
     public void deleteTemplate(int id){
-        int i = 0;
+        db.templateDao().deleteTemplate(templates.get(id));
+        templates.remove(id);
 
-        for(; i< templates.size(); i++){
-            if( templates.get(i).getId() == id){
-                db.templateDao().deleteTemplate(templates.get(i));
-                templates.remove(i);
-                break;
-            }
-        }
-
-        for(; i < templates.size(); i++){
+        for(int i = id; i < templates.size(); i++){
             templates.get(i).setId(templates.get(i).getId() - 1);
             db.templateDao().updateTemplate(templates.get(i));
         }
     }
 
-    public void editTemplate(int id, boolean isIncome, int amount, String description, Group group){
-        for (Template template : templates) {
-            if (template.getId() == id) {
-                template.setIncome(isIncome);
-                template.setAmount(amount);
-                template.setGroup(group);
-                template.setDescription(description);
-                db.templateDao().updateTemplate(template);
-            }
+    public void editTemplate(Template template, boolean isIncome, int amount, String description, Group group){
+        template.setIncome(isIncome);
+        template.setAmount(amount);
+        template.setDescription(description);
+        template.setGroup(group);
+        db.templateDao().updateTemplate(template);
+    }
+
+    public void convertTemplate(Template template){
+        if( template.isIncome() ){
+            addIncome(template.getAmount(), template.getDescription());
+        }
+        else{
+            addExpense(template.getAmount(), template.getDescription(), template.getGroupId());
         }
     }
 }
