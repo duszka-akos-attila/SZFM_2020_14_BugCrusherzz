3fd5be89e217695710cd39e4cc9d4e3e163881df
raczandras <raczandras0204@gmail.com>
2020-09-27 12:16:20 +0200

    Optimized the mainActivity and manager classes.

--- Tracky/Application/Tracky/app/src/main/java/com/tracky/MainActivity.java
+++ Tracky/Application/Tracky/app/src/main/java/com/tracky/MainActivity.java
@@ -7,8 +7,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import android.content.Context;
+import android.os.Bundle;
+import com.google.android.material.navigation.NavigationView;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import com.example.tracky.data.manager.Manager;
+
+
 public class MainActivity extends AppCompatActivity {
 
+    private AppBarConfiguration mAppBarConfiguration;
     public static Context context;
 
     @Override
@@ -16,12 +30,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         context = getApplicationContext();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_income, R.id.nav_expense, R.id.nav_group, R.id.nav_template, R.id.nav_summary)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        Manager.init();
     }
 
     public void onDevModePressed(View view){
         Intent intent = new Intent(MainActivity.context,TestActivity.class);
         intent.addFlags(intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
         this.startActivity(intent);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 
 }
--- Tracky/Application/Tracky/app/src/main/java/com/tracky/data/manager/Manager.java
+++ Tracky/Application/Tracky/app/src/main/java/com/tracky/data/manager/Manager.java
@@ -1,13 +1,15 @@
 package com.tracky.data.manager;
 
+import android.util.Log;
+
 import androidx.room.Room;
 
-import com.tracky.MainActivity;
-import com.tracky.data.Expense;
-import com.tracky.data.Group;
-import com.tracky.data.Income;
-import com.tracky.data.Template;
-import com.tracky.data.base.AppDatabase;
+import com.example.tracky.data.Expense;
+import com.example.tracky.data.Income;
+import com.example.tracky.MainActivity;
+import com.example.tracky.data.Group;
+import com.example.tracky.data.Template;
+import com.example.tracky.data.base.AppDatabase;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -16,48 +18,50 @@ import java.util.List;
 
 public class Manager{
 
-    AppDatabase db = Room.databaseBuilder(MainActivity.context, AppDatabase.class, "TrackyDB").allowMainThreadQueries().build();
+    static AppDatabase db = Room.databaseBuilder(MainActivity.context , AppDatabase.class, "TrackyDB").allowMainThreadQueries().build();
 
-    List<Income> dbIncomes= new ArrayList<Income>();
-    List<Expense> dbExpanses = new ArrayList<Expense>();
-    List<Template> dbTemplates = new ArrayList<Template>();
-    List<Group> dbGroups = new ArrayList<Group>();
+    public static List<Income> dbIncomes = new ArrayList<Income>();
+    public static List<Expense> dbExpenses = new ArrayList<Expense>();
+    public static List<Template> dbTemplates = new ArrayList<Template>();
+    public static List<Group> dbGroups = new ArrayList<Group>();
 
-
-    public void importDB() {
+    public static void importDB(){
         dbIncomes = db.incomeDao().selectAllIncome();
-        dbExpanses = db.expenseDao().selectAllExpense();
+        dbExpenses = db.expenseDao().selectAllExpense();
         dbTemplates = db.templateDao().selectAllTemplate();
         dbGroups = db.groupDao().selectAllGroup();
     }
 
-    public List<Income> getDbIncomes() {
+    public static List<Income> getDbIncomes() {
         return dbIncomes;
     }
 
-    public List<Expense> getDbExpanses() {
-        return dbExpanses;
+    public static List<Expense> getDbExpenses() {
+        return dbExpenses;
     }
 
-    public List<Template> getDbTemplates() {
+    public static List<Template> getDbTemplates() {
         return dbTemplates;
     }
 
-    public List<Group> getDbGroups() {
+    public static List<Group> getDbGroups() {
         return dbGroups;
     }
+
 
     //------------------------------------INCOMES----------------------------
 
 
-    public void addIncome(int amount, String description){
+    public static void addIncome(int amount, String description){
         int id = dbIncomes.size();
         dbIncomes.add(new Income(id, amount, description, Calendar.getInstance().getTime() ));
         db.incomeDao().insertIncome(new Income(id, amount, description, Calendar.getInstance().getTime()));
+        for( Income i : dbIncomes){
+            Log.i("adatb",i.toString());
+        }
     }
 
-    public void deleteIncome(int id){
-
+    public static void deleteIncome(int id){
         db.incomeDao().deleteIncome(dbIncomes.get(id));
         dbIncomes.remove(id);
 
@@ -68,7 +72,7 @@ public class Manager{
         }
     }
 
-    public void editIncome( Income income, int amount, String description, Date date){
+    public static void editIncome( Income income, int amount, String description, Date date){
         income.setAmount(amount);
         income.setDescription(description);
         income.setDate(date);
@@ -78,23 +82,23 @@ public class Manager{
     //--------------------------------EXPENSE-------------------------------
 
 
-    public void addExpense(int amount, String description, int groupId){
-        int id = dbExpanses.size();
-        dbExpanses.add(new Expense(id, amount, description, groupId, Calendar.getInstance().getTime() ));
+    public static void addExpense(int amount, String description, int groupId){
+        int id = dbExpenses.size();
+        dbExpenses.add(new Expense(id, amount, description, groupId, Calendar.getInstance().getTime() ));
         db.expenseDao().insertExpense(new Expense(id, amount, description, groupId, Calendar.getInstance().getTime()));;
     }
 
-    public void deleteExpense(int id){
-        db.expenseDao().deleteExpense(dbExpanses.get(id));
-        dbExpanses.remove(id);
+    public static void deleteExpense(int id){
+        db.expenseDao().deleteExpense(dbExpenses.get(id));
+        dbExpenses.remove(id);
 
-        for(int i = id; i < dbExpanses.size(); i++){
-            dbExpanses.get(i).setId(dbExpanses.get(i).getId() - 1);
-            db.expenseDao().updateExpense(dbExpanses.get(i));
+        for(int i = id; i < dbExpenses.size(); i++){
+            dbExpenses.get(i).setId(dbExpenses.get(i).getId() - 1);
+            db.expenseDao().updateExpense(dbExpenses.get(i));
         }
     }
 
-    public void editExpense( Expense expense, int amount, String description, Group group, Date date){
+    public static void editExpense( Expense expense, int amount, String description, Group group, Date date){
         expense.setAmount(amount);
         expense.setDescription(description);
         expense.setGroup(group);
@@ -106,13 +110,13 @@ public class Manager{
     //--------------------------------------GROUPS------------------------
 
 
-    public void addGroup(String name, int color){
+    public static void addGroup(String name, int color){
         int id = dbGroups.size();
         dbGroups.add(new Group(id, name, color));
         db.groupDao().insertGroup(new Group(id, name, color));
     }
 
-    public void deleteGroup(int id){
+    public static void deleteGroup(int id){
         db.groupDao().deleteGroup(dbGroups.get(id));
         dbGroups.remove(id);
 
@@ -123,7 +127,7 @@ public class Manager{
         }
     }
 
-    public void editGroup(Group group, String name, int color){
+    public static void editGroup(Group group, String name, int color){
 
         group.setName(name);
         group.setColor(color);
@@ -133,13 +137,13 @@ public class Manager{
     //--------------------------------------Templates------------------------
 
 
-    public void addTemplate(boolean isIncome, int amount, String description, int groupId){
+    public static void addTemplate(boolean isIncome, int amount, String description, int groupId){
         int id = dbTemplates.size();
         dbTemplates.add(new Template(isIncome, id, amount, description, groupId));
         db.templateDao().insertTemplate(new Template(isIncome, id, amount, description, groupId));
     }
 
-    public void deleteTemplate(int id){
+    public static void deleteTemplate(int id){
         db.templateDao().deleteTemplate(dbTemplates.get(id));
         dbTemplates.remove(id);
 
@@ -149,7 +153,7 @@ public class Manager{
         }
     }
 
-    public void editTemplate(Template template, boolean isIncome, int amount, String description, Group group){
+    public static void editTemplate(Template template, boolean isIncome, int amount, String description, Group group){
         template.setIncome(isIncome);
         template.setAmount(amount);
         template.setDescription(description);
@@ -157,7 +161,7 @@ public class Manager{
         db.templateDao().updateTemplate(template);
     }
 
-    public void convertTemplate(Template template){
+    public static void convertTemplate(Template template){
         if( template.isIncome() ){
             addIncome(template.getAmount(), template.getDescription());
         }
@@ -166,19 +170,157 @@ public class Manager{
         }
     }
 
-    public List<Income> getIncomes() {
-        return dbIncomes;
+    public static int getBalance(){
+        int balance = 0;
+
+        for (Income i : dbIncomes){
+            balance += i.getAmount();
+        }
+
+        for (Expense e : dbExpenses){
+            balance -= e.getAmount();
+        }
+
+        return balance;
     }
 
-    public List<Expense> getExpenses() {
-        return dbExpanses;
+    public static List<Income> getIncomeResults( int day, int month, int amount) {
+        ArrayList<Income> results = new ArrayList<>();
+
+        if( day == 0 && month == 0 && amount == 0){
+            return dbIncomes;
+        }
+        else if( day != 0 && month != 0 && amount != 0){
+            for( Income i : dbIncomes ){
+                if( i.getDate().getMonth() == month && i.getDate().getDate() == day && i.getAmount() == amount ){
+                    results.add(i);
+                }
+            }
+            return results;
+        }
+        else if( day != 0 && month != 0 ){
+            for( Income i : dbIncomes ){
+                if( i.getDate().getMonth() == month && i.getDate().getDate() == day){
+                    results.add(i);
+                }
+            }
+            return results;
+        }
+        else if( day != 0 && amount != 0 ){
+            for( Income i : dbIncomes ){
+                if( i.getDate().getDate() == day && i.getAmount() == amount ){
+                    results.add(i);
+                }
+            }
+            return results;
+        }
+        else if( amount != 0 && month != 0 ){
+            for( Income i : dbIncomes ){
+                if( i.getDate().getMonth() == month && i.getAmount() == amount ){
+                    results.add(i);
+                }
+            }
+            return results;
+        }
+
+        else if( amount !=0 ){
+            for( Income i : dbIncomes ){
+                if( i.getAmount() == amount ){
+                    results.add(i);
+                }
+            }
+            return results;
+        }
+
+        else if( month !=0 ){
+            for( Income i : dbIncomes ){
+                if( i.getDate().getMonth() == month){
+                    results.add(i);
+                }
+            }
+            return results;
+        }
+
+        else if( day !=0 ){
+            for( Income i : dbIncomes ){
+                if( i.getDate().getDate() == day ){
+                    results.add(i);
+                }
+            }
+            return results;
+        }
+
+        return results;
     }
 
-    public List<Template> getTemplates() {
-        return dbTemplates;
+    public static List<Expense> getExpenseResults(int day, int month, int amount) {
+        ArrayList<Expense> results = new ArrayList<>();
+
+        if( day == 0 && month == 0 && amount == 0){
+            return dbExpenses;
+        }
+        else if( day != 0 && month != 0 && amount != 0){
+            for( Expense e : dbExpenses ){
+                if( e.getDate().getMonth() == month && e.getDate().getDate() == day && e.getAmount() == amount ){
+                    results.add(e);
+                }
+            }
+            return results;
+        }
+        else if( day != 0 && month != 0 ){
+            for( Expense e : dbExpenses ){
+                if( e.getDate().getMonth() == month && e.getDate().getDate() == day){
+                    results.add(e);
+                }
+            }
+            return results;
+        }
+        else if( day != 0 && amount != 0 ){
+            for( Expense e : dbExpenses ){
+                if( e.getDate().getDate() == day && e.getAmount() == amount ){
+                    results.add(e);
+                }
+            }
+            return results;
+        }
+        else if( amount != 0 && month != 0 ){
+            for( Expense e : dbExpenses ){
+                if( e.getDate().getMonth() == month && e.getAmount() == amount ){
+                    results.add(e);
+                }
+            }
+            return results;
+        }
+
+        else if( amount !=0 ){
+            for( Expense e : dbExpenses ){
+                if( e.getAmount() == amount ){
+                    results.add(e);
+                }
+            }
+            return results;
+        }
+
+        else if( month !=0 ){
+            for( Expense e : dbExpenses ){
+                if( e.getDate().getMonth() == month){
+                    results.add(e);
+                }
+            }
+            return results;
+        }
+
+        else if( day !=0 ){
+            for( Expense e : dbExpenses ){
+                if( e.getDate().getDate() == day ){
+                    results.add(e);
+                }
+            }
+            return results;
+        }
+
+        return results;
     }
 
-    public List<Group> getGroups() {
-        return dbGroups;
-    }
+
 }
