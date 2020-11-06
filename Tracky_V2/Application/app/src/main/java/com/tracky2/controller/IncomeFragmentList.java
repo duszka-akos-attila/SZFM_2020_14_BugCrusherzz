c681706d60ee5ee5b07a541a9ad19f6f87a6229f
raczandras <raczandras0204@gmail.com>
2020-11-06 18:09:31 +0100

    Started working on the controller's create income, edit income and delete income methods

--- Tracky_V2/Application/app/src/main/java/com/tracky2/controller/IncomeFragmentList.java
+++ Tracky_V2/Application/app/src/main/java/com/tracky2/controller/IncomeFragmentList.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import com.tracky2.R;
+import com.tracky2.data.manager.Manager;
 
 public class IncomeFragmentList extends Fragment {
 
@@ -16,4 +19,12 @@ public class IncomeFragmentList extends Fragment {
 
         return root;
     }
+
+    //TODO kódrészlet beillesztése a listenerekbe
+
+    /*Bevétel törlése kódrészlet
+        Lekérni a bevétel objektumot és
+        Manager.deleteIncome(income);
+        Toast.makeText(getContext(), "Sikeres törlés!", Toast.LENGTH_SHORT).show();
+     */
 }
