2ecc4729246011fb8ac44229361e75973f29e4bc
raczandras <raczandras0204@gmail.com>
2020-11-06 18:08:43 +0100

    Started working on the controller's create expense, edit expense and delete expense methods

--- Tracky_V2/Application/app/src/main/java/com/tracky2/controller/ExpenseFragment.java
+++ Tracky_V2/Application/app/src/main/java/com/tracky2/controller/ExpenseFragment.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import com.tracky2.R;
+import com.tracky2.data.manager.Manager;
 
 public class ExpenseFragment extends Fragment {
 
@@ -16,4 +19,44 @@ public class ExpenseFragment extends Fragment {
 
         return root;
     }
+
+
+    //TODO beilleszteni az alábbi kódokat a listenerekbe
+
+    /*
+    Új KIADÁS hozzáadásához kódrészlet
+
+    if( KIADÁSMENNYISÉGMEZŐJE.getText().length() == 0){
+        Toast.makeText(getContext(), "Üres a Mennyiség mező!", Toast.LENGTH_SHORT).show();
+    }
+    else if(|| KIADÁSCSOPORTMEZŐJE.getText().length() == 0){
+        Toast.makeText(getContext(), "Üres a kiadás csoportja mező!", Toast.LENGTH_SHORT).show();
+    }
+    else {
+        try {
+            int amount = Integer.parseInt(String.valueOf(KIADÁSMENNYISÉGMEZŐJE.getText()));
+            if( amount < 0){
+                Toast.makeText(getContext(), amount + "A megadott érték kevesebb mint nulla!", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Manager.addExpense(amount, KIADÁSLEÍRÁSMEZŐJE.getText().toString(), Integer.parseInt(String.valueOf(KIADÁSCSOPORTJAMEZŐ.getText())));
+                Toast.makeText(getContext(), amount + " hozzáadva a kiadásokhoz!", Toast.LENGTH_SHORT).show();
+                KIADÁSMENNYISÉGEMEZŐ.setText("");
+                KIADÁSLEÍRÁSAMEZŐ.setText("");
+                KIADÁSCSOPORTJAMEZŐ.setText("");
+            }
+        } catch (java.lang.NumberFormatException e) {
+            Toast.makeText(getContext(), KIADÁSMENNYISÉGEMEZŐ.getText().toString() + " nem egy szám!", Toast.LENGTH_SHORT).show();
+        }
+    }
+     */
+
+    /*Kiadás szerkesztése kódrészlet
+        Lekérni a kiadás objektumot az új értékekkel és
+        Manager.editExpense(expense);
+        Toast.makeText(getContext(), "Sikeres szerkesztés!", Toast.LENGTH_SHORT).show();
+     */
+
+
+
 }
--- Tracky_V2/Application/app/src/main/java/com/tracky2/controller/ExpenseFragmentList.java
+++ Tracky_V2/Application/app/src/main/java/com/tracky2/controller/ExpenseFragmentList.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import com.tracky2.R;
+import com.tracky2.data.manager.Manager;
 
 public class ExpenseFragmentList extends Fragment {
 
@@ -16,4 +19,12 @@ public class ExpenseFragmentList extends Fragment {
 
         return root;
     }
+
+    //TODO Ezeket a függvényeket beilleszteni a listenerekbe
+
+    /*Kiadás törlése kódrészlet
+        Lekérni a kiadás objektumot és
+        Manager.deleteExpense(expense);
+        Toast.makeText(getContext(), "Sikeres törlés!", Toast.LENGTH_SHORT).show();
+     */
 }
