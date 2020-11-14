ee92c22009ccf26d142f1618ee5c09528614dbf0
Duszka Ákos Attila <duszka.akos.attila@gmail.com>
2020-11-14 02:59:37 +0100

    Created the frames of the methods, which filter and order the Incomes.

--- Tracky_V2/Application/app/src/main/java/com/tracky2/data/analysis/SearchAndFilter.java
+++ Tracky_V2/Application/app/src/main/java/com/tracky2/data/analysis/SearchAndFilter.java
@@ -1,7 +1,28 @@
 package com.tracky2.data.analysis;
 
+import com.tracky2.data.Income;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 public class SearchAndFilter {
 
     // ------------------------- VARIABLES -------------------------
-    
+
+    // ------------------------- INCOME FILTERING -------------------------
+
+    public ArrayList<Income> incomesBetweenAmounts(int min, int max){
+
+    }
+
+    public ArrayList<Income> incomesBetweenDates(Date min, Date max){
+
+    }
+
+    // ------------------------- INCOME SORTING -------------------------
+
+    public ArrayList<Income> sortIncomes(ArrayList<Income> incomes, String by, String direction){
+
+    }
+
 }
