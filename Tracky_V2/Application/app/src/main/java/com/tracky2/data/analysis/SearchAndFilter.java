ece94fab54cc3b86a0fef20bd78126b0922ca5e8
Duszka Ákos Attila <duszka.akos.attila@gmail.com>
2020-11-14 03:42:43 +0100

    Finished the sorting method for incomes.

--- Tracky_V2/Application/app/src/main/java/com/tracky2/data/analysis/SearchAndFilter.java
+++ Tracky_V2/Application/app/src/main/java/com/tracky2/data/analysis/SearchAndFilter.java
@@ -3,6 +3,8 @@ package com.tracky2.data.analysis;
 import com.tracky2.data.Income;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 
 public class SearchAndFilter {
@@ -22,7 +24,43 @@ public class SearchAndFilter {
     // ------------------------- INCOME SORTING -------------------------
 
     public ArrayList<Income> sortIncomes(ArrayList<Income> incomes, String by, String direction){
+        if(by.equals("date")) {
+            if(direction.equals("asc")) {
+                Collections.sort(incomes, new Comparator<Income>() {
+                    public int compare(Income i1, Income i2) {
+                        return i1.getDate().compareTo(i2.getDate());
+                    }
+                });
+            }
+            if(direction.equals("desc")) {
+                Collections.sort(incomes, new Comparator<Income>() {
+                    public int compare(Income i1, Income i2) {
+                        return i2.getDate().compareTo(i1.getDate());
+                    }
+                });
+            }
 
+        }
+
+        if(by.equals("amount")) {
+            if(direction.equals("asc")) {
+                Collections.sort(incomes, new Comparator<Income>() {
+                    public int compare(Income i1, Income i2) {
+                        return Integer.compare(i1.getAmount(), i2.getAmount());
+
+                    }
+                });
+            }
+            if(direction.equals("desc")) {
+                Collections.sort(incomes, new Comparator<Income>() {
+                    public int compare(Income i1, Income i2) {
+                        return Integer.compare(i2.getAmount(), i1.getAmount());
+                    }
+                });
+            }
+
+        }
+        return incomes;
     }
 
 }
