31679609636439f3a314f06e6c79d6e998d9b05c
Drcsonka <halasz.adam99@gmail.com>
2020-11-14 04:34:42 +0100

    Made the Income and Expense tables

--- Tracky_V2/Application/app/src/main/java/com/tracky2/controller/ExpenseFragmentList.java
+++ Tracky_V2/Application/app/src/main/java/com/tracky2/controller/ExpenseFragmentList.java
@@ -8,16 +8,35 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.tracky2.R;
 import com.tracky2.data.manager.Manager;
+import com.tracky2.mainTablaAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExpenseFragmentList extends Fragment {
+
+    RecyclerView MainTabla;
+    mainTablaAdapter MainTablaAdapter;
+
+    List<String> balanceList;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_expenselist, container, false);
 
+        balanceList = new ArrayList<>();
+        //balanceList = Manager.lastBalanceModificaitons(20,"-","auto");
+        MainTabla = root.findViewById(R.id.mainTablaExp);
+        MainTablaAdapter = new mainTablaAdapter(balanceList);
+
+        MainTabla.setAdapter(MainTablaAdapter);
         return root;
+
     }
 
     //TODO Ezeket a függvényeket beilleszteni a listenerekbe
--- Tracky_V2/Application/app/src/main/java/com/tracky2/controller/IncomeFragmentList.java
+++ Tracky_V2/Application/app/src/main/java/com/tracky2/controller/IncomeFragmentList.java
@@ -8,16 +8,47 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.tracky2.R;
 import com.tracky2.data.manager.Manager;
+import com.tracky2.mainTablaAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class IncomeFragmentList extends Fragment {
+
+    RecyclerView MainTabla;
+    mainTablaAdapter MainTablaAdapter;
+
+    List<String> balanceList;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_incomelist, container, false);
 
+        balanceList = new ArrayList<>();
+        //balanceList = Manager.lastBalanceModificaitons(20,"-","auto");
+        MainTabla = root.findViewById(R.id.mainTablaInc);
+        MainTablaAdapter = new mainTablaAdapter(balanceList);
+
+/*
+        FloatingActionButton fab = root.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new IncomeFragment();
+                FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.income_frag, fragment);
+            }
+        });*/
+
+
+        MainTabla.setAdapter(MainTablaAdapter);
         return root;
+
     }
 
     //TODO kódrészlet beillesztése a listenerekbe
