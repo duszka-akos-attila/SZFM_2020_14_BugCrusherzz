5312fc1dcbe3090927e7e3a74be1329eb54371d0
Duszka Ákos Attila <duszka.akos.attila@gmail.com>
2020-12-06 17:58:13 +0100

    Remade the structure of the ExpenseFragmentList.

--- Tracky_V2/Application/app/src/main/java/com/tracky2/controller/ExpenseFragmentList.java
+++ Tracky_V2/Application/app/src/main/java/com/tracky2/controller/ExpenseFragmentList.java
@@ -1,5 +1,6 @@
 package com.tracky2.controller;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,25 +9,39 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.tracky2.R;
+import com.tracky2.adapters.mainExpenseListRecycleAdapter;
+import com.tracky2.data.Expense;
+import com.tracky2.data.manager.Manager;
 import com.tracky2.expTablaAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ExpenseFragmentList extends Fragment {
 
     RecyclerView MainTabla;
     expTablaAdapter expTablaAdapter;
 
+    //New expense list
+    RecyclerView mainExpenseListRecycleView;
+    mainExpenseListRecycleAdapter mainExpenseListRecycleAdapter;
+
     List<String> ExpbalanceList;
+
+    List<Expense> expenses;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_expenselist, container, false);
+        View root = inflater.inflate(R.layout.fragment_expenselist2, container, false);
+
+        /*
 
         ExpbalanceList = new ArrayList<>();
         //balanceList = Manager.lastBalanceModificaitons(20,"-","auto");
@@ -37,9 +52,64 @@ public class ExpenseFragmentList extends Fragment {
 
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(MainTabla.getContext(), DividerItemDecoration.VERTICAL);
         MainTabla.addItemDecoration(dividerItemDecoration);
+
+
+         */
+
+
+        //New expense list
+        expenses = Manager.getExpensesDescDate();
+        mainExpenseListRecycleView = root.findViewById(R.id.mainExpenseListRecycleView);
+        mainExpenseListRecycleAdapter = new mainExpenseListRecycleAdapter(expenses);
+        mainExpenseListRecycleView.setAdapter(mainExpenseListRecycleAdapter);
+        DividerItemDecoration decoration = new DividerItemDecoration(mainExpenseListRecycleView.getContext(), DividerItemDecoration.VERTICAL);
+        mainExpenseListRecycleView.addItemDecoration(decoration);
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(mainExpenseListRecycleView);
+
         return root;
 
     }
+
+    Expense deletedExpense;
+
+    ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+            final int position = viewHolder.getAdapterPosition();
+
+            switch (direction){
+                case ItemTouchHelper.LEFT:
+                    deletedExpense = expenses.get(position);
+                    Manager.deleteExpense(expenses.get(position));
+                    expenses.remove(position);
+                    mainExpenseListRecycleAdapter.notifyItemRemoved(position);
+                    Snackbar.make(mainExpenseListRecycleView, "A "+deletedExpense.getDescription()+" leírású elem törölve lett.",Snackbar.LENGTH_LONG)
+                            .setAction("Visszavonás", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    expenses.add(position, deletedExpense);
+                                    mainExpenseListRecycleAdapter.notifyItemInserted(position);
+                                    Manager.addExpense(deletedExpense);
+                                }
+                            }).show();
+                    break;
+
+                case ItemTouchHelper.RIGHT:
+
+                    break;
+
+            }
+
+        }
+    };
 
     //TODO Ezeket a függvényeket beilleszteni a listenerekbe
 
